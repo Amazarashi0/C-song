3715a8c865226490257d613c1989f4589bfe79d8
0vers <2803175913s@gmail.com>
2020-09-05 17:19:12 +0800

    master

--- Single-Work/C/Quadratic-equation-with-one-unknown.c
+++ Single-Work/C/Quadratic-equation-with-one-unknown.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-#include<math.h>
+# include <stdio.h>
+# include <math.h>
 int main (void)
 {
-    int a=1,b=2,c=4;
-    float delta;
-    delta=b*b-4*a*c;
-    float x1,x2;//存放解的值
-    if (delta>0)
+    int a = 1, b = 2, c = 1;
+    double delta;
+    delta = b*b - 4*a*c; // 适当加空格使代码更加清晰
+    double x1, x2; // 存放解的值
+    if (delta > 0)
     {
-        x1=(-b+sqrt(delta))/(2*a);
-        x2=(-b-sqrt(delta))/(2*a);
-        printf("该一元二次方程有两个解，x1=%f,x2=%f\n",x1,x2);
+        x1 = (-b + sqrt(delta)) / (2*a);
+        x2 = (-b - sqrt(delta)) / (2*a);
+        printf("2j，x1 = %f, x2 = %f\n", x1, x2); // 该一元二次方程有两个解
     }
-    else if (delta==0)
+    else if (delta == 0)
     {
-        x1=(-b)/(2*a);
-        x2=x1;
-        printf("该一元二次方程两个相等的解，x1=x2=%f\n",x1,x2);
+        x1 = (-b) / (2*a);
+        x2 = x1;
+        printf("1j，x1 = x2 = %f\n", x1, x2); // 该一元二次方程两个相等的解
     }
-    else if (delta<0)
+    else if (delta < 0)
     {
-        printf("无解\n");
+        printf("wj\n"); // 无解
     }
 }
