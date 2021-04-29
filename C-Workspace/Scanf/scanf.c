18f55d7fb6316a8680e2ae20a85f39e361bef408
0vers <2803175913s@gmail.com>
2021-04-29 16:51:27 +0800

    master

--- C-Workspace/Scanf/scanf.c
+++ C-Workspace/Scanf/scanf.c
@@ -2,10 +2,10 @@
 int main ()
 {
     int i;
-    scanf("%d", &i);  //55s
-    printf("%d\n", i); //55
-    scanf("%c", &i);  //s
-    printf("%c\n", i);  //s
+    scanf("%d", &i);  //：55s
+    printf("%d\n", i); //：55，只读取整型数据55的代码并输出；
+    scanf("%c", &i);  //：s
+    printf("%c\n", i);  //：s
 
     return 0;
 }
